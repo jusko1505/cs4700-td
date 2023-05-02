0975911123c9c464ecf484c128cc20aed41c3582
jusko1505 <justinkousa@gmaillcom>
2023-05-02 14:45:09 -0700

    moved some things around

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class GameOverScript : MonoBehaviour
 {
+    public string mainMenuScene = "MainMenu";
+    
     /* script that should contain what the buttons on the main menu screen should do */
     // Start is called before the first frame update
     void Start()
@@ -15,5 +19,15 @@ public class GameOverScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void onRetry(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Debug.Log("retry");
+    }
+
+    public void onMainMenu(){
+        SceneManager.LoadScene(mainMenuScene);
+        Debug.Log("Going to menu");
     }
 }
